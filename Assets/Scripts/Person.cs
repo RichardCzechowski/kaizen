59eece2585e6fbc7408e8de59de66ec8acdb406c
Wil Gieseler <wil@wilgieseler.com>
2016-01-29 20:30:51 -0800

    line renderer

--- Assets/Scripts/Person.cs
+++ Assets/Scripts/Person.cs
@@ -8,18 +8,24 @@ public class Person : MonoBehaviour {
 
 	private Transform _currentDestination;
 	private NavMeshAgent _agent;
-
+	private LineRenderer _lineRenderer;
 
 	void Start () {
 		_agent = GetComponent<NavMeshAgent>();
 		_currentDestination = objects[0];
 		_agent.destination = _currentDestination.position; 
+		_lineRenderer = GetComponentInChildren<LineRenderer> ();
+		_lineRenderer.useWorldSpace = true;
 	}
 	
 	void Update () {
 		if (Vector3.Distance(_currentDestination.position, transform.position) < 1) {
 			MoveToNext ();
 		}
+
+		_lineRenderer.SetVertexCount (_agent.path.corners.Length);
+		_lineRenderer.SetPositions (_agent.path.corners);
+
 	}
 
 	void MoveToNext() {
@@ -33,7 +39,7 @@ public class Person : MonoBehaviour {
 		_agent.destination = _currentDestination.position; 
 	}
 
-	void OnDrawGizmos() {
+	void OnDrawGizmosSelected() {
 		foreach (var o in objects) {
 			if (o == _currentDestination) {
 				Gizmos.color = Color.green;
@@ -42,6 +48,17 @@ public class Person : MonoBehaviour {
 				Gizmos.color = Color.red;
 			}
 			Gizmos.DrawWireSphere (o.position, 1);
+		}
+
+		if (_agent) {
+			Gizmos.color = Color.blue;
+			for (var i = 0; i < _agent.path.corners.Length; i ++) {
+				Gizmos.DrawWireSphere (_agent.path.corners[i], 0.5f);
+				if (i < _agent.path.corners.Length - 1) {
+					Gizmos.DrawLine (_agent.path.corners [i], _agent.path.corners [i + 1]);
+				}
+			}
+		
 		}
 	}
 
