12482eaa7c24de28c9d745a5a6887f1538fdaa16
Richard Tanner <dicktanner@gmail.com>
2016-01-29 22:11:59 -0800

    Add state machine to player script

--- Assets/Scripts/Person.cs
+++ Assets/Scripts/Person.cs
@@ -1,7 +1,11 @@
 ﻿using UnityEngine;
 using System;
+using System.Collections;
 
 public class Person : MonoBehaviour {
+	
+	public enum State {settingPath, waiting, walking};
+	public State state = State.settingPath;
 
 	public Transform[] objects;
 
@@ -62,4 +66,30 @@ public class Person : MonoBehaviour {
 		}
 	}
 
+	///////////////////// STATE MACHINE
+
+	public void setState (State newState) {
+		state = newState;
+		switch(state){
+		case State.settingPath:
+			// Don't move until path is set
+			Debug.Log("settingPath");
+			setState(State.waiting);
+			break;
+		case State.waiting:
+			//Hangout for a length of time
+			Debug.Log("waiting");
+			StartCoroutine(wait(10));
+			break;
+		case State.walking:
+			// Animate walking
+			break;
+		}
+	}
+
+	IEnumerator wait(int time) {
+		Debug.Log("Before Waiting 2 seconds");
+		yield return new WaitForSeconds(time);
+		Debug.Log("After Waiting 2 Seconds");
+	}
 }
---
+++ Assets/personState.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class personState : MonoBehaviour {
+	public enum State {settingPath, waiting, walking};
+	public State state = State.settingPath;
+
+	// Use this for initialization
+	void Start () {
+		setState (State.waiting);
+	}
+	
+	// Update is called once per frame
+	public void setState (State newState) {
+		state = newState;
+		switch(state){
+		case State.settingPath:
+			// Don't move until path is set
+			Debug.Log("settingPath");
+			setState(State.waiting);
+			break;
+		case State.waiting:
+			//Hangout for a length of time
+			Debug.Log("waiting");
+			StartCoroutine(wait(10));
+			break;
+		case State.walking:
+			// Animate walking
+			break;
+		}
+	}
+
+	IEnumerator wait(int time) {
+		Debug.Log("Before Waiting 2 seconds");
+		yield return new WaitForSeconds(time);
+		Debug.Log("After Waiting 2 Seconds");
+	}
+
+}
