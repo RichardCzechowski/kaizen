8118f59331715537c1cdd5c9fa366bd201a627ff
Richard Tanner <dicktanner@gmail.com>
2016-01-29 21:14:14 -0800

    Add game manager

---
+++ Assets/pathManager.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class pathManager : MonoBehaviour {
+	public bool startNewPath;
+	private Ray ray;
+	private int i = 0;
+	private GameObject pathToSet;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetMouseButtonDown(0)) {
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit;
+			// Casts the ray and get the first game object hit
+			Physics.Raycast(ray, out hit);
+			if (hit.transform != null && hit.transform.gameObject.tag == "Player") {
+				i = 0;
+				startNewPath = true;
+				pathToSet = hit.transform.gameObject;
+			}else if (hit.transform != null && startNewPath && hit.transform.gameObject.tag == "Building") {
+				i++;
+				if (i < pathToSet.GetComponent<setNewPath>().path.Length) {
+					pathToSet.GetComponent<setNewPath>().path[i] = hit.transform;
+					Debug.Log (pathToSet);
+					if (i == 6) {
+						startNewPath = false;
+					}
+				}
+			}
+		}
+	}
+}
--- Assets/setNewPath.cs
+++ Assets/setNewPath.cs
@@ -3,9 +3,6 @@ using System.Collections;
 
 public class setNewPath : MonoBehaviour {
 	public Transform[] path;
-	public bool startNewPath;
-	private Ray ray;
-	private int i = 0;
 
 	// Use this for initialization
 	void Start () {
@@ -13,22 +10,6 @@ public class setNewPath : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (startNewPath == true && Input.GetMouseButtonDown(0)) {
-			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-			RaycastHit hit;
-			// Casts the ray and get the first game object hit
-			Physics.Raycast(ray, out hit);
-			if (hit.transform != null && hit.transform.gameObject.tag == "building") {
-				i++;
-				if (i < path.Length) {
-					path [i] = hit.transform;
-					Debug.Log (path);
-				}
-			}
-		}
 	}
-
-	void OnMouseDown () {
-		startNewPath = true;
-	}
+		
 }
