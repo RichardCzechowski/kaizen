f535f6cc1ac758018d9a2a91c84b28f4b4bd40ba
Wil Gieseler <wil@wilgieseler.com>
2016-01-30 13:00:24 -0800

    time of day preview beginnings

--- Assets/Scripts/NightLight.cs
+++ Assets/Scripts/NightLight.cs
@@ -5,21 +5,21 @@ public class NightLight : MonoBehaviour {
 
 	public float randomness = 0.05f;
 
-	Light light;
+	Light _light;
 	float _sunrise;
 	float _sunset;
 	void Start() {
-		light = GetComponent<Light> ();
+		_light = GetComponent<Light> ();
 		_sunrise = 0.3f + Random.value * randomness - randomness;
 		_sunset = 0.7f + Random.value * randomness - randomness;
 	}
 
 	void Update () {
-		if (DayNightController.instance.currentTimeOfDay < _sunrise || DayNightController.instance.currentTimeOfDay > _sunset) {
-			light.enabled = true;
+		if (DayNightController.instance.TimeOfDayIncludingPreview() < _sunrise || DayNightController.instance.TimeOfDayIncludingPreview() > _sunset) {
+			_light.enabled = true;
 		
 		} else {
-			light.enabled = false;
+			_light.enabled = false;
 
 		}
 	}
