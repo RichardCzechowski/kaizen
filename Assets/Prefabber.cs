c67e69002f3e34d9ecf0d464df3bd4f4c4727048
Wil Gieseler <wil@wilgieseler.com>
2016-01-30 17:46:50 -0800

    refactor occupancy indicator

---
+++ Assets/Prefabber.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Prefabber : MonoBehaviour {
+
+	public GameObject prefab;
+
+	void Start () {
+		GameObject obj = Instantiate (prefab, transform.position, transform.rotation) as GameObject;
+		obj.transform.parent = transform;
+	}
+
+}
