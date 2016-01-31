e5856dd7e7b744a4d2ac9c3d27e0fb678f0c4266
Wil Gieseler <wil@wilgieseler.com>
2016-01-31 02:10:46 -0800

    status icon faces camera

---
+++ Assets/FaceCamera.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FaceCamera : MonoBehaviour {
+
+	void Update() {
+		Vector3 targetPostition = new Vector3( Camera.main.transform.position.x, 
+			this.transform.position.y, 
+			Camera.main.transform.position.z ) ;
+		this.transform.LookAt( targetPostition ) ;
+	}
+
+}
